cdd9bff3f35213f35638f4cdc0c1b8deb7ad761c
MARAT <mark9205@mail.ru>
2022-06-10 17:23:32 +0300

    first commit to reactTableTask

---
+++ src/data/data.js
@@ -0,0 +1,86 @@
+export const data = [
+	{
+		date: 2021,
+		points: 2,
+		name: 'Mark',
+		distance: 1000
+	},
+	{
+		date: 2022,
+		points: 4,
+		name: 'Dmitry',
+		distance: 850
+	},
+	{
+		date: 2020,
+		points: 3,
+		name: 'Anna',
+		distance: 121
+	},
+	{
+		date: 2019,
+		points: 1,
+		name: 'Lana',
+		distance: 103
+	},
+	{
+		date: 2019,
+		points: 1,
+		name: 'Viktor',
+		distance: 323
+	},
+	{
+		date: 2018,
+		points: 6,
+		name: 'Jannet',
+		distance: 240
+	},
+	{
+		date: 2014,
+		points: 2,
+		name: 'Maria',
+		distance: 1115
+	},
+	{
+		date: 2015,
+		points: 4,
+		name: 'Shaldon',
+		distance: 25
+	},
+	{
+		date: 2016,
+		points: 3,
+		name: 'Mika',
+		distance: 30
+	},
+	{
+		date: 2014,
+		points: 3,
+		name: 'Georgy',
+		distance: 35
+	},
+	{
+		date: 2018,
+		points: 2,
+		name: 'Yury',
+		distance: 33
+	},
+	{
+		date: 2013,
+		points: 1,
+		name: 'Mickail',
+		distance: 65
+	},
+	{
+		date: 2020,
+		points: 10,
+		name: 'Maks',
+		distance: 332
+	},
+	{
+		date: 2022,
+		points: 20,
+		name: 'Niko',
+		distance: 133
+	},
+];
